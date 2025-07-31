8b4c0e4679526953fda3ef682c4f3d99deac7d38
lee-lou2 <lee@lou2.kr>
2025-07-31 10:51:21 +0900

    mod, CSRF_TRUSTED_ORIGINS

--- conf/settings.py
+++ conf/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 INSTALLED_APPS = [
     "unfold",
